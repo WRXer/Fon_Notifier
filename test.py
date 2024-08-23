806bec17e96945d09c4aaf94787065591118488b
WRXer <wrxerrr@gmail.com>
2024-08-23 11:10:27 +0400

    настроил частоту проверки

--- test.py
+++ test.py
@@ -35,6 +35,7 @@ async def send_message():
 
 
 async def schedule_next_message():
+    await asyncio.sleep(20)    # Ждем 10 секунд перед отправкой следующего сообщения
     await send_message()    # Отправляем сообщение и снова планируем следующую задачу
 
 
